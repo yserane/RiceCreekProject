c363708e9acaf54997e06a2419acbe3881282de7
yserane <55660730+yserane@users.noreply.github.com>
2021-04-15 02:22:34 -0400

    fixibg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,49 +27,49 @@ export class HomeComponent implements OnInit {
   showImageTextConainter: boolean = false;
   cards: CardTextImageModel[] = [
     {
-      image: '../assets/bottom-image/IMG2.JPG',
+      image: '../assets/bottom-image/IMG2.png',
       imageName: ' ',
       customStyle: "img-custom-size"
     },
     {
-      image: '../assets/bottom-image/IMG3.JPG',
-      imageName: ' ',
-      customStyle: "img-custom-size"
-
-    },
-    {
-      image: '../assets/bottom-image/IMG4.JPG',
-      imageName: ' ',
-      customStyle: "img-custom-size"
-    },
-    {
-      image: '../assets/bottom-image/IMG5.JPG',
-      imageName: ' ',
-      customStyle: "img-custom-size"
-    },
-    {
-      image: '../assets/bottom-image/IMG6.JPG',
+      image: '../assets/bottom-image/IMG3.png',
       imageName: ' ',
       customStyle: "img-custom-size"
 
     },
     {
-      image: '../assets/bottom-image/IMG7.JPG',
+      image: '../assets/bottom-image/IMG4.png',
       imageName: ' ',
       customStyle: "img-custom-size"
     },
     {
-      image: '../assets/bottom-image/IMG8.JPG',
+      image: '../assets/bottom-image/IMG5.png',
       imageName: ' ',
       customStyle: "img-custom-size"
     },
     {
-      image: '../assets/bottom-image/IMG9.JPG',
+      image: '../assets/bottom-image/IMG6.png',
+      imageName: ' ',
+      customStyle: "img-custom-size"
+
+    },
+    {
+      image: '../assets/bottom-image/IMG7.png',
       imageName: ' ',
       customStyle: "img-custom-size"
     },
     {
-      image: '../assets/bottom-image/IMG1.JPG',
+      image: '../assets/bottom-image/IMG8.png',
+      imageName: ' ',
+      customStyle: "img-custom-size"
+    },
+    {
+      image: '../assets/bottom-image/IMG9.png',
+      imageName: ' ',
+      customStyle: "img-custom-size"
+    },
+    {
+      image: '../assets/bottom-image/IMG1.png',
       imageName: ' ',
       customStyle: "img-custom-size"
     }];
@@ -79,7 +79,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.imageMapHashMap.set(1, {
-      image: '../assets/map-images/Map_1.JPG',
+      image: '../assets/map-images/Map_1.png',
       imageName: 'Black-capped Chickadee | SUNY Oswego Student',
       customStyle: "img-custom-size-smaller"
     },
@@ -91,37 +91,37 @@ export class HomeComponent implements OnInit {
       },
     );
     this.imageMapHashMap.set(3, {
-        image: '../assets/map-images/Map_3.JPG',
+        image: '../assets/map-images/Map_3.png',
         imageName: 'Kingfisher | SUNY Oswego Student',
           customStyle: "img-custom-size-smaller"
       },
     );
     this.imageMapHashMap.set(4, {
-      image: '../assets/map-images/Map_4.JPG',
+      image: '../assets/map-images/Map_4.png',
       imageName:'Meadow Scene | SUNY Oswego Student',
         customStyle: "img-custom-size-smaller"
       },
     );
     this.imageMapHashMap.set(5, {
-      image: '../assets/map-images/Map_5.JPG',
+      image: '../assets/map-images/Map_5.png',
         imageName: 'Woodpecker | SUNY Oswego Student',
         customStyle: "img-custom-size-smaller"
       },
     );
     this.imageMapHashMap.set(6, {
-      image: '../assets/map-images/Map_6.JPG',
+      image: '../assets/map-images/Map_6.png',
         imageName: 'Meadow Scene | SUNY Oswego Student',
         customStyle: "img-custom-size-smaller"
       },
     );
     this.imageMapHashMap.set(7, {
-      image: '../assets/map-images/Map_7.JPG',
+      image: '../assets/map-images/Map_7.png',
         imageName: 'Flower Study | SUNY Oswego Student',
         customStyle: "img-custom-size-smaller"
       },
     );
     this.imageMapHashMap.set(8, {
-      image: '../assets/map-images/Map_8.JPG',
+      image: '../assets/map-images/Map_8.png',
 
         imageName: 'Nuthatch | SUNY Oswego Student',
         customStyle: "img-custom-size-smaller"
